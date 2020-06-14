fe9d5bea351d1d362f46848413855c8323ff5049
aymeric-giraudet <neadj59@gmail.com>
2020-06-14 23:42:49 +0200

    2 files modified

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import './style.css';
 import Tone from "tone"
 import { Midi } from "@tonejs/midi";
+import toWav from "audiobuffer-to-wav";
 
 var pulseOptions = {
   oscillator:{
@@ -48,35 +49,39 @@ var inputElement = document.getElementById('midiFile');
 inputElement.addEventListener("change", handleFiles, false);
 
 async function playMidi(file) {
-  // load a midi file in the browser
-  const midi = new Midi(file)
-  //the file name decoded from the first track
-  const name = midi.name
-  const now = Tone.now() + 0.5
-  //get the tracks
-  midi.tracks.forEach(track => {
-    let synth;
-    switch(track.name) {
-      case "p1":
-        synth = pulseSynth;
+  Tone.offline(function (Transport) {
+      // load a midi file in the browser
+    const midi = new Midi(file)
+    //the file name decoded from the first track
+    const name = midi.name
+    const now = Tone.now() + 0.5
+    //get the tracks
+    midi.tracks.forEach(track => {
+      let synth;
+      switch(track.name) {
+        case "p1":
+          synth = pulseSynth;
+          break;
+        case "p2":
+          synth = squareSynth;
+          break;
+        case "tr":
+          synth = triangleSynth;
+          break;
+        default:
+        synth = noiseSynth;
         break;
-      case "p2":
-        synth = squareSynth;
-        break;
-      case "tr":
-        synth = triangleSynth;
-        break;
-      default:
-       synth = noiseSynth;
-       break;
-    }
-    console.log(track)
-    //tracks have notes and controlChanges
+      }
+      console.log(track)
+      //tracks have notes and controlChanges
 
-    //notes are an array
-    const notes = track.notes
-    notes.forEach(note => {
-      synth.triggerAttackRelease(note.name, note.duration, note.time + now, note.velocity)
+      //notes are an array
+      const notes = track.notes
+      notes.forEach(note => {
+        synth.triggerAttackRelease(note.name, note.duration, note.time + now, note.velocity)
+      })
     })
-  })
+  }).then(buffer => {
+    const wav = toWav(buffer);
+  });
 }
